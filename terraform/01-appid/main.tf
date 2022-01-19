4fe19da3fed664e42658596d6470454851e83d7d
Henrik Loeser <hloeser@de.ibm.com>
2022-01-19 17:35:20 +0100

    tweak App ID config

--- terraform/01-appid/main.tf
+++ terraform/01-appid/main.tf
@@ -28,6 +28,7 @@ resource "ibm_appid_idp_cloud_directory" "clouddirectory" {
   tenant_id = ibm_resource_instance.WS_AppID.guid
   is_active = true
   identity_field = "email"
+  identity_confirm_access_mode = "OFF"
   self_service_enabled = false
   signup_enabled = false
   welcome_enabled = false
